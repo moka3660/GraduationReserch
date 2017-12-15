2f772e91d989312574b775429b31e6c3e334c1a3
moka3660 <moka12almukha30@gmail.com>
2017-12-15 14:05:24 +0900

    update thanks.php
    DBに接続

--- thanks.php
+++ thanks.php
@@ -8,6 +8,12 @@
 <body>
   thanks
 <?php
+  $dsn = 'mysql:dbname=phpkiso;host=localhost';
+  $user = 'root';
+  $password = '';
+  $dbh = new PDO($dsn,$user,$password);
+  $dbh->query('SET NAMES utf8');
+
   $nickname=$_POST['nickname'];
   $naiyou=$_POST['naiyou'];
   $hitokoto=$_POST['hitokoto'];
@@ -20,6 +26,12 @@
   print $naiyou;
   print '<br>';
   print $hitokoto;
+
+  $sql = 'INSERT INTO anketo (nickname,naiyou,hitokoto)VALUES("'.$nickname.'","'.$naiyou.'","'.$hitokoto.'")';
+  $stmt = $dbh->prepare($sql);
+  $stmt->execute();
+
+  $dbh = null;
  ?>
 </body>
 </html>
